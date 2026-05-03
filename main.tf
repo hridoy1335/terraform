bcb50c36934592321ded2630c281f0210cdd4a77
Hridoy Khan <hridoykinfo@gmail.com>
2026-05-03 12:22:17 +0600

    another things push on create storage accounts

--- main.tf
+++ main.tf
@@ -12,3 +12,16 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_storage_account" "st" {
+  name                     = "tesings1335"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "development"
+    name = "Hridoy Khan"
+  }
+}
