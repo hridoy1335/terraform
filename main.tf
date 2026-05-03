f461c7c84de18e7811fe30b4486c67e80c7198b1
Hridoy Khan <hridoykinfo@gmail.com>
2026-05-03 12:04:42 +0600

    testing

--- main.tf
+++ main.tf
@@ -5,3 +5,10 @@ resource "azurerm_resource_group" "rg" {
     name = "Hridoy Khan"
   }
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  address_space       = var.virtual_network_prefix
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
